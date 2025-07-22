79bdab6e91083bf8c890f5d75e5f33373d762ea1
Matt Perry <mattgperry@gmail.com>
2025-07-22 11:55:41 +0200

    Fixing types

--- packages/motion-dom/src/animation/utils/make-animation-instant.ts
+++ packages/motion-dom/src/animation/utils/make-animation-instant.ts
@@ -1,7 +1,10 @@
 import { ValueAnimationOptions } from "../types"
 
 export function makeAnimationInstant(
-    options: Partial<ValueAnimationOptions>
+    options: Partial<{
+        duration: ValueAnimationOptions["duration"]
+        type: ValueAnimationOptions["type"]
+    }>
 ): void {
     options.duration = 0
     options.type === "keyframes"
