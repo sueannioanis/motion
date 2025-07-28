9387425918bb3f2ec5e316cf4f3160c7ac044b6b
Matt Perry <mattgperry@gmail.com>
2025-07-28 10:13:19 +0200

    Fixing projection

--- packages/framer-motion/src/projection/node/create-projection-node.ts
+++ packages/framer-motion/src/projection/node/create-projection-node.ts
@@ -1901,7 +1901,6 @@ export function createProjectionNode<I>({
             targetStyle: any, // CSSStyleDeclaration - doesn't allow numbers to be assigned to properties
             styleProp?: MotionStyle
         ) {
-            console.log("applyProjectionStyles")
             if (!this.instance || this.isSVG) return
 
             if (!this.isVisible) {
@@ -1960,7 +1959,7 @@ export function createProjectionNode<I>({
             }
 
             targetStyle.transform = transform
-            console.log("transform", transform)
+
             const { x, y } = this.projectionDelta
             targetStyle.transformOrigin = `${x.origin * 100}% ${
                 y.origin * 100
